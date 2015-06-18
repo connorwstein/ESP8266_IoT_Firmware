7cfe48c1ca068eaa4531f8fcacb782b1d62502e6
connorwstein <connor.stein@mail.mcgill.ca>
2015-06-18 17:27:12 -0400

    testing the temperature sensor AM2303

--- user/temperature.c
+++ user/temperature.c
@@ -3,21 +3,17 @@
 #include "ip_addr.h"
 #include "espconn.h"
 
-#include "debug.h"
 
 static int temperature = 0;
 
 void ICACHE_FLASH_ATTR Temperature_set_temperature(int temp)
 {
-	DEBUG("enter Temperature_set_temperature");
 	temperature = temp;
 	ets_uart_printf("Set TEMPERATURE to %d\n", temperature);
-	DEBUG("exit Temperature_set_temperature");
 }
 
 void ICACHE_FLASH_ATTR Temperature_get_temperature(struct espconn *conn)
 {
-	DEBUG("enter Temperature_get_temperature");
 	char buff[20];
 
 	os_sprintf(buff, "%d", temperature);
@@ -26,5 +22,4 @@ void ICACHE_FLASH_ATTR Temperature_get_temperature(struct espconn *conn)
 		ets_uart_printf("espconn_sent failed.\n");
 
 	ets_uart_printf("Replied with: %s\n", buff);
-	DEBUG("exit Temperature_get_temperature");
 }
