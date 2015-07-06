5444c301170acae7a624dcc238a309c6321e6efe
test <ernacktob@yahoo.ca>
2015-07-06 10:22:51 -0400

    added esf_buf

--- esp8266_wifi_raw/disas/lmac/lmac.c
+++ esp8266_wifi_raw/disas/lmac/lmac.c
@@ -16,95 +16,58 @@ void lmacProcessRtsStart(uint8 arg1)
 }
 
 /* 0x401024cc */
-void lmacTxFrame(uint32 arg1, uint8 arg2)
+void lmacTxFrame(uint8 *arg1, uint8 arg2)
 {
 	$a2 = arg1;
 	$a3 = arg2;
-	$a0 = (uint8 *)&lmacConfMib + 44;	/* 0x3ffe8e80 */
-	$a12 = 9 * $a3;
-	$a12 = 4 * $a12 + $a0;
-	$a0 = ((uint8 *)$a12)[17];
+
+	$a12 = (uint8 *)&lmacConfMib + 44 + 36 * arg2;
+	$a0 = ((uint8 *)&lmacConfMib + 44 + 36 * arg2)[17];
 
 	if ($a0 == 0 || $a0 == 3) {
-		((uint32 *)$a12)[0] = $a2;
+		((uint8 **)&lmacConfMib + 11 + 9 * arg2)[0] = arg1;
 
-		if ($a2 == 0) {
-			ets_printf("%s %u\n", "lmac.c", 1513);
+		if (arg1 == NULL) {
+			ets_printf("%s %u\n", "lmac.c", 1519);
 			while (1);
 		}
 
-		$a8 = 0x100;
-		$a6 = ((uint16 *)$a2)[11];
-		$a5 = ((uint16 *)$a2)[10];
-		$a7 = 0x03ffefff;
-		$a5 += $a6;
-		$a6 = ((uint32 *)$a2)[9];
-		$a4 = (uint8 *)&lmacConfMib - 0x4;
-		$a3 = ((uint32 *)$a6)[0];
-		$a4 = ((uint16 *)$a4)[7];
-		$a0 = $a3 >> 6;
+		$a5 = ((uint16 *)arg1)[10] + ((uint16 *)arg1)[11];
+		$a6 = ((uint32 **)arg1)[9];
+		$a3 = ((uint32 **)arg1)[9][0];
+		$a4 = ((uint16 *)&lmacConfMib)[5];
 
 		if (($a4 < $a5) && !($a3 & (1 << 7))) {
-			$a5 = $a0 | $a8;
-			$a4 = $a3 & 0x3f;
-			$a5 <<= 6;
-			$a4 |= $a5;
-			$a3 = $a4 & 0x3f;
-			$a4 >>= 6;
-			$a4 &= $a7;
-			$a4 <<= 6;
-			$a3 |= $a4;
-			((uint32 *)$a6)[0] = $a3;
-			$a0 = $a3 >> 6;
+			/* Set bit 14 of $a3, clear bit 18 of $a3.
+			   Done in a convoluted way by the compiler. */
+			((uint32 **)arg1)[9][0] |= (1 << 14);
+			((uint32 **)arg1)[9][0] &= 0xfffbffff;
 		}
 
-		if (($a0 & (1 << 12))) {
-			$a9 = ((uint8 *)$a12)[17];
+		/* Again, this this was done in a convoluted way by the compiler,
+		   perhaps for optimization or processor quirks...? */
+		if ($a3 & (1 << 18)) {
+			$a9 = ((uint8 *)&lmacConfMib + 44 + 36 * arg2)[17];
 
 			if ($a9 == 3) {
-				$a10 = ((uint8 *)$a6)[4];
-				$a10 >>= 4;
-
-				if ($a10 >= 3) {
-					$a4 = 0x0000f03f;
-					$a9 = $a3 & 0x3f;
-					$a5 = ((uint8 *)$a6)[5];
-					$a10 = $a0 | $a8;
-					$a10 <<= 6;
-					$a11 = $a5 & 0x3f;
-					$a9 |= $a10;
-					$a10 = $a9 >> 6;
-					$a10 &= $a7;
-					$a9 &= 0x3f;
-					$a10 <<= 6;
-					$a9 |= $a10;
-					$a10 = ((uint8 *)$a6)[6];
-					$a11 <<= 6;
-					$a10 <<= 8;
-					$a10 |= $a5;
-					$a10 &= $a4;
+				if ((((uint8 **)arg1)[9][4] >> 4) >= 3) {
+					$a5 = ((uint8 **)arg1)[9][5];
+					$a11 = ($a5 & 0x3f) << 6;
+					$a10 = (((uint8 **)arg1)[9][6] << 8) | ((uint8 **)arg1)[9][5];
+					$a10 &= 0x0000f03f;
 					$a10 |= $a11;
-					((uint8 *)$a6)[5] = $a10;
-					$a10 >>= 8;
-					((uint8 *)$a6)[6] = $a10;
-					$a4 = ((uint32 *)$a2)[9];
-					((uint32 *)$a6)[0] = $a9;
-					$a11 = ((uint8 *)$a4)[5];
-					$a5 = 192;
-					$a11 &= $a5;
-					((uint8 *)$a4)[5] = $a11;
+					((uint8 **)arg1)[9][5] = $a10;
+					((uint8 **)arg1)[9][6] = $a10 >> 8;
+					((uint32 **)arg1)[9][0] |= (1 << 14);
+					((uint32 **)arg1)[9][0] &= 0xfffbffff;
+					((uint8 **)arg1)[9][5] &= 0xc0;
 				}
 			}
 		}
 
-		$a2 = $a12;
-		$a3 = 0;
-		_0x401012ac();	/* <lmacIsIdle+0xb4> */
+		_0x401012ac($a12, 0);	/* <lmacIsIdle+0xb4> */
 	} else if ($a0 == 4) {
-		$a3 = ((uint32 *)$a12)[0];
-		$a3 -= $a2;
-
-		if ($a3 != 0) {
+		if (arg1 != ((uint8 **)&lmacConfMib + 11 + 9 * arg2)[0]) {
 			ets_printf("%s %u\n", "lmac.c", 1514);
 			while (1);
 		}
@@ -113,18 +76,12 @@ void lmacTxFrame(uint32 arg1, uint8 arg2)
 		while (1);
 	}
 
-	$a4 = 0x3ff20e00;
 	$a7 = ((uint8 *)$a12)[6];
-	$a6 = 1;
-	$a5 = $a6 << $a7;
-	$a5 -= 1;
-	$a2 = ((volatile uint8 *)$a12)[4];
-	$a3 = ((volatile uint8 *)$a12)[5];
-	$a4 = ((uint32 *)$a4)[17];
-	((uint8 *)$a12)[17] = $a6;
-	$a4 &= $a5;
+	$a5 = (1 << $a7) - 1;
+	$a4 = ((volatile uint32 *)0x3ff20e00)[17] & $a5;
+	((uint8 *)$a12)[17] = 1;
 	$a4 &= 0xffff;
-	wDev_EnableTransmit();
+	wDev_EnableTransmit(((uint8 *)$a12)[4], ((uint8 *)$a12)[5], $a4);
 }
 
 /* 0x40102680 */
