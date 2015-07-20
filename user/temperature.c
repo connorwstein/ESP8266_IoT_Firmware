7286c84d436a9ecd4780510223bc98ce90f530ae
connorwstein <connor.stein@mail.mcgill.ca>
2015-07-20 16:56:16 -0400

    updated dht driver to work on new hardware configuration with battery

--- user/temperature.c
+++ user/temperature.c
@@ -6,12 +6,22 @@
 #include "gpio.h"
 #include "debug.h"
 #include "mem.h"
+#include "user_interface.h"
 
 #include "device_config.h"
 
-#define INITIAL_DELAY 2000000
-#define INITIATE_PROTOCOL_DELAY 20000
+#define INITIAL_DELAY 200000 
+#ifndef HIGH 
+#define HIGH 1
+#endif
+#ifndef LOW 
+#define LOW 0
+#endif
+#define GPIO2 2
+#define T_BE 20000 //Tbe in data sheet
+#define T_GO 40 //Tgo in data sheet
 #define MAX_READ_TIME 4000
+#define PROTOCOL_BITS 1 //received bits to ignore (part of the protocol and not the data, varies on the chips due to timing, might be 2 on some chips)
 #define READ_BUFFER_SIZE 42 //buffer for the 40 data bits, with some extra space for the initial response signals
 #define DATA_BIT_HIGH_THRESHOLD 18 //number of ones in a row that indicates a bit high, 
 //note that this depends on the code in the while loop of the read function, if that loop takes longer to execute i.e. code is added
@@ -35,7 +45,7 @@ void ICACHE_FLASH_ATTR set_dht(unsigned char* data){
 	//40 bytes representing 40 bits--> 40 bits i.e. 5 bytes
 	unsigned short data_compressed[2];
 	memset(data_compressed,0,sizeof data_compressed);
-	data+=2;
+	data+=1;
 	if(data[0]==1){
 		DHT.negative_temp=true;
 		ets_uart_printf("Negative temperature detected\n");
@@ -53,7 +63,7 @@ void ICACHE_FLASH_ATTR set_dht(unsigned char* data){
 
 
 bool ICACHE_FLASH_ATTR checksum(unsigned char *data){
-	data+=2; //skip the first 2 protocol bits
+	data+=1; //skip the first 2 protocol bits
 	unsigned char data_bytes[5]; //40 data bits --> 5 bytes
 	memset(data_bytes,0,sizeof data_bytes);
 	int i;
@@ -77,6 +87,7 @@ unsigned char* ICACHE_FLASH_ATTR get_data_bits(unsigned char *raw_data, int size
 	int data_index=0;
 	int data_bit_high_threshold=DATA_BIT_HIGH_THRESHOLD; //18 ones in a row or more and you have a 1
 	bool ones_streak=false;
+	ets_uart_printf("DATA: \n");
 	for(i=0;i<size;i++){
 		//loop through the array of ones and zeros
 		if(data_index>=READ_BUFFER_SIZE) break;
@@ -109,15 +120,13 @@ int ICACHE_FLASH_ATTR read(void){
 	int k;
 	unsigned char *raw_data=(unsigned char*)os_zalloc(MAX_READ_TIME*sizeof(char));
     memset(raw_data,0,sizeof raw_data);
-    GPIO_OUTPUT_SET(2, 1);
-    ets_uart_printf("GPIO2 %d for 20ms\n",GPIO_INPUT_GET(2)); //READS pin 1=high 3.3V
-    os_delay_us(INITIATE_PROTOCOL_DELAY); //Keep it high for a while before initiating.
-    GPIO_OUTPUT_SET(2, 0);
-    ets_uart_printf("GPIO2 %d for 20ms\n",GPIO_INPUT_GET(2)); //READS pin 1=high 3.3V
-    os_delay_us(INITIATE_PROTOCOL_DELAY); //Tbe in data sheet. Host the start signal down time. Using the max of 20ms.
-    GPIO_DIS_OUTPUT(2); //When you disable output mode (i.e. switch to input), the pull up resistor on the SDA data will cause the line to go high 
-    //ets_uart_printf("GPIO2 %d delay for 40us\n",GPIO_INPUT_GET(2)); //READS pin 1=high 3.3V
-    //os_delay_us(40); //Tgo in data sheet. Bus master has released time
+    GPIO_OUTPUT_SET(GPIO2, HIGH);
+    os_delay_us(INITIAL_DELAY); //Keep it high for a while before initiating.
+    GPIO_OUTPUT_SET(GPIO2, LOW);
+    os_delay_us(T_BE); //Tbe in data sheet. Host the start signal down time. Using the max of 20ms.
+    GPIO_OUTPUT_SET(GPIO2,HIGH); 
+    os_delay_us(T_GO);
+    GPIO_DIS_OUTPUT(GPIO2); //When you disable output mode (i.e. switch to input), the pull up resistor on the SDA data will cause the line to go high 
     //Wait for pin to drop, sensor takes over right here 
     //Now should send response signal that goes Low 80us then High 80us 
     //(the sensor response signal)
@@ -128,13 +137,12 @@ int ICACHE_FLASH_ATTR read(void){
   	//Maximum, 40 1s would be 120us *40 =4800us, 4800 sets off the watch dog however,
   	//just use 4000us (very unlikely to be all 1s anyways)
   	while(j++<MAX_READ_TIME){
-  		raw_data[j]=GPIO_INPUT_GET(2);
+  		raw_data[j]=0x1&GPIO_INPUT_GET(GPIO2);
   		os_delay_us(1);
   	}
-  	ets_uart_printf("DEVICE DONE\n");
   	// for(k=0;k<MAX_READ_TIME;k++){
-  	// 	ets_uart_printf("%d",raw_data[k]);
-  	// }
+  	//  	ets_uart_printf("%d",raw_data[k]);
+  	//  }
   	unsigned char* data=get_data_bits(raw_data,MAX_READ_TIME);
   	if(!checksum(data)){
   		goto fail;
@@ -142,6 +150,9 @@ int ICACHE_FLASH_ATTR read(void){
   	else{
   		set_dht(data);
   	}
+  	ets_uart_printf("Freeing buffers\n");
+  	os_free(raw_data);
+  	os_free(data);
   	return 0;
     
     fail: 
@@ -157,9 +168,7 @@ void ICACHE_FLASH_ATTR Temperature_get_temperature(struct espconn *conn)
 	//defined in eagle_soc.h PERIPHS_IO_MUX_GPIO2_U --> (PERIPHS_IO_MUX + 0x38)
 	//FUNC_GPIO2 -->  0
 	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2); 
-	ets_uart_printf("GPIO2 after PIN_FUNC_SELECT: %d\n",GPIO_INPUT_GET(BIT2)); //READS pin 1=high 3.3V
     PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO2_U);	
-	ets_uart_printf("GPIO2 after PIN_PULLUP_EN: %d\n",GPIO_INPUT_GET(BIT2)); //READS pin 1=high 3.3V
 	char buff[10];
 	if(read()!=-1){
 		if(DHT.negative_temp){
@@ -169,8 +178,6 @@ void ICACHE_FLASH_ATTR Temperature_get_temperature(struct espconn *conn)
 		else{
 			os_sprintf(buff,"%d %d",DHT.humidity,DHT.temperature);
 		}
-		
-		ets_uart_printf("Buff %s\n",buff);
 		ets_uart_printf("Sending Humidity: %d Temperature: %d\n",DHT.humidity,DHT.temperature);
 		espconn_sent(conn, (uint8 *)buff, 10);
 	}
@@ -181,6 +188,7 @@ void ICACHE_FLASH_ATTR Temperature_get_temperature(struct espconn *conn)
 
 int ICACHE_FLASH_ATTR Temperature_init(struct DeviceConfig *config)
 {
+	gpio_init();
 	return 0;
 }
 
